d350bdf106ee4360e79a9b4d1fc88b155d7ff613
Thorhian <thorhian@protonmail.com>
2020-09-26 21:52:41 -0600

    Finished prove02, included decent css and book deletion.

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')))
    .use('/ta03', ta03Routes) 
    .use('/ta04', ta04Routes)
    .use('/prove01', prove01Routes)
+   .use('/prove02', prove02Routes)
    .get('/', (req, res, next) => {
      // This is the primary index, always handled last. 
      res.render('pages/index', {title: 'Welcome to my CSE341 repo', path: '/'});
